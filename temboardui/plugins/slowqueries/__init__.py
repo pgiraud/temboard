5d54faf8a14d4ccb1c6082924166dc0c0f40332f
Pierre GIRAUD <pierre.giraud@dalibo.com>
2019-05-24 17:02:25 +0200

    Prepare slowqueries plugin

---
+++ temboardui/plugins/slowqueries/__init__.py
@@ -0,0 +1,41 @@
+import logging
+from os.path import realpath
+
+from temboardui.web import (
+    Blueprint,
+    TemplateRenderer,
+)
+
+
+blueprint = Blueprint()
+blueprint.generic_proxy(r"/slowqueries")
+logger = logging.getLogger(__name__)
+plugin_path = realpath(__file__ + '/..')
+render_template = TemplateRenderer(plugin_path + '/templates')
+
+
+def configuration(config):
+    return {}
+
+
+def get_routes(config):
+    routes = blueprint.rules
+    return routes
+
+
+@blueprint.instance_route(r"/slowqueries")
+def slowqueries(request):
+    request.instance.check_active_plugin(__name__)
+
+    try:
+        agent_username = request.instance.get_profile()['username']
+    except Exception:
+        agent_username = None
+
+    return render_template(
+        'slowqueries.html',
+        nav=True, role=request.current_user,
+        instance=request.instance,
+        agent_username=agent_username,
+        plugin=__name__,
+    )
