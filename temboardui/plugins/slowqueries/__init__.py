4456fdb568c8f9b6260f7ae992debda5264bce61
Pierre GIRAUD <pierre.giraud@dalibo.com>
2019-05-29 09:59:44 +0200

    Slow queries settings

--- temboardui/plugins/slowqueries/__init__.py
+++ temboardui/plugins/slowqueries/__init__.py
@@ -4,6 +4,8 @@ from os.path import realpath
 
 from temboardui.web import (
     Blueprint,
+    HTTPError,
+    Redirect,
     TemplateRenderer,
 )
 
@@ -43,4 +45,50 @@ def slowqueries(request):
         instance=request.instance,
         agent_username=agent_username,
         plugin=__name__,
+    )
+
+
+@blueprint.instance_route(r"/slowqueries/settings",
+                          methods=["GET", "POST"])
+def settings(request):
+    request.instance.check_active_plugin(__name__)
+    profile = request.instance.get_profile()
+    agent_username = profile['username']
+    template_vars = {}
+    configuration_status = {
+        'restart_pending': False
+    }
+    settings_url = '/slowqueries/settings'
+
+    if "GET" == request.method:
+        settings = request.instance.get(settings_url)
+    else:
+        settings = {'settings': [
+            {'name': name, 'setting': value[0]}
+            for name, value in request.arguments.iteritems()
+            # 'filter' is not a setting, just ignore it.
+            if name != 'filter'
+        ]}
+        try:
+            request.instance.post(settings_url, body=settings)
+            # Redirect to GET page, same URI.
+            return Redirect(request.uri)
+        except HTTPError as e:
+            # Rerender HTML page with errors.
+            template_vars['error_code'] = e
+            template_vars['error_message'] = e.log_message
+
+    plugin_path = realpath(__file__ + '/..')
+    # we use the pgconf template here
+    render_template = TemplateRenderer(plugin_path + '/../pgconf/templates')
+    return render_template(
+        'configuration.html',
+        nav=True, role=request.current_user,
+        instance=request.instance,
+        agent_username=agent_username,
+        plugin=__name__,
+        xsession=request.instance.xsession,
+        configuration_categories=False,
+        configuration_status=configuration_status,
+        data=settings,
     )
